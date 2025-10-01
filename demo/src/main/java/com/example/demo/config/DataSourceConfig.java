0d8bb3d87f7af26362e1151d81820b082a401430
Shabarivasan K <shabarivasankaruppannan@gmail.com>
2025-10-01 17:34:08 +0530

    fix

--- demo/src/main/java/com/example/demo/config/DataSourceConfig.java
+++ demo/src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -14,10 +15,16 @@ import javax.sql.DataSource;
 @EnableConfigurationProperties(DataSourceProperties.class)
 public class DataSourceConfig {
 
+    @Value("${spring.datasource.hikari.pool-name:NewsHikariCP}")
+    private String poolName;
+
     @Bean
     @Primary
     public DataSource dataSource(DataSourceProperties properties) {
         HikariConfig config = new HikariConfig();
+        
+        // Set pool name first to avoid conflicts with environment variables
+        config.setPoolName(poolName);
         
         // Configure Hikari with the same properties as in application.properties
         config.setJdbcUrl(properties.determineUrl());
@@ -26,7 +33,6 @@ public class DataSourceConfig {
         config.setDriverClassName(properties.determineDriverClassName());
         
         // Set Hikari properties
-        config.setPoolName("NewsHikariCP");
         config.setConnectionTimeout(20000);
         config.setMinimumIdle(5);
         config.setMaximumPoolSize(15);
